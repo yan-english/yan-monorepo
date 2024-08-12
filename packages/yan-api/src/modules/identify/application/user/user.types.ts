66d1229abde5247b669ca0ac064b9a3e89b66dfa
Binh Nguyen Van <nvbinhsoft@gmail.com>
2024-08-13 01:25:01 +0700

    feat: create value objects for user

---
+++ packages/yan-api/src/modules/identify/application/user/user.types.ts
@@ -0,0 +1,12 @@
+// All properties that a UserEntity has
+export interface UserProps {
+  role: string;
+  username: string;
+  password: string;
+}
+
+// Properties that are needed for a user creation
+export interface CreateUserProps {
+  username: string;
+  password: string;
+}
---
+++ packages/yan-api/src/modules/identify/domain/value-objects/password.vo.ts
@@ -0,0 +1,20 @@
+export class PasswordVo {
+  private readonly value: string;
+
+  constructor(value: string) {
+    if (!this.validate(value)) {
+      // Need to update this logic. Throw a custom error
+      throw new Error('Invalid password');
+    }
+
+    this.value = value;
+  }
+
+  getValue(): string {
+    return this.value;
+  }
+  private validate(password: string): boolean {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    return passwordRegex.test(password);
+  }
+}
