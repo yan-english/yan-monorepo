12bbbdc9aaa380538f361799d6b2133323e73bb6
Binh Nguyen Van <nvbinhsoft@gmail.com>
2024-08-13 00:28:34 +0700

    feat: add typeORM config

--- packages/yan-api/src/app.module.ts
+++ packages/yan-api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './modules/identify/infrastructure/database/configs/typeorm.config';
+import { IdentifyModule } from './modules/identify/identify.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), IdentifyModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ packages/yan-api/src/modules/identify/infrastructure/database/configs/typeorm.config.ts
@@ -0,0 +1,13 @@
+// src/config/typeorm.config.ts
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'mydatabase',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true, // Set to false in production
+};
