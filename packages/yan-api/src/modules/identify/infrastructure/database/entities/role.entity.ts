825ea97d802fb925cf955345f84d835a38546f27
nvbinhsoft <nvbinhsoft@gmail.com>
2024-08-14 14:50:02 +0700

    feat: create entities for identity module

---
+++ packages/yan-api/src/modules/identify/infrastructure/database/entities/role.entity.ts
@@ -0,0 +1,21 @@
+import {Column, Entity, PrimaryGeneratedColumn, OneToMany} from "typeorm";
+import {UserRoleEntity} from "./user-role.entity";
+import {RolePermissionEntity} from "./role-permission.entity";
+
+@Entity("roles")
+export class RoleEntity {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column({ unique: true })
+    name: string;
+
+    @Column()
+    description: string;
+
+    @OneToMany(() => UserRoleEntity, userRole => userRole.role)
+    userRoles: UserRoleEntity[];
+
+    @OneToMany(() => RolePermissionEntity, rolePermission => rolePermission.role)
+    rolePermissions: RolePermissionEntity[];
+}
---
+++ packages/yan-api/src/modules/identify/infrastructure/database/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import {Column, Entity, OneToMany, PrimaryColumn, PrimaryGeneratedColumn} from "typeorm";
+import {UserRoleEntity} from "./user-role.entity";
+
+@Entity("users")
+export class UserEntity {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    username: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({ type: 'date', name: 'date_of_birth' })
+    dateOfBirth: Date;
+
+    // consider use enum
+    @Column()
+    gender: string;
+
+    @OneToMany(() => UserRoleEntity, userRole => userRole.user)
+    userRoles: UserRoleEntity[];
+
+}
