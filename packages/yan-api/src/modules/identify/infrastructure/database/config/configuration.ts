a19de0204071e72d50fcf63a7a08a67f75613cd4
Binh Nguyen Van <nvbinhsoft@gmail.com>
2024-08-19 23:37:46 +0700

    chore: change database port

--- packages/yan-api/src/modules/identify/infrastructure/database/config/configuration.ts
+++ packages/yan-api/src/modules/identify/infrastructure/database/config/configuration.ts
@@ -1,5 +1,5 @@
 export const DBConfiguration = () => ({
-  DATABASE_PORT: process.env.DATABASE_PORT || '5443',
+  DATABASE_PORT: process.env.DATABASE_PORT || '5432',
   DATABASE_HOST: process.env.DATABASE_HOST || 'localhost',
   DATABASE_USERNAME: process.env.DATABASE_USERNAME || 'user',
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'password',
