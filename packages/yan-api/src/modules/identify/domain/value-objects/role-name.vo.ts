e829e5c44470734f9d4f0132abe9118f28e4030b
nvbinhsoft <nvbinhsoft@gmail.com>
2024-08-21 18:16:25 +0700

    feat: implement create role

---
+++ packages/yan-api/src/modules/identify/domain/value-objects/role-name.vo.ts
@@ -0,0 +1,12 @@
+
+export class RoleName {
+    private readonly value: string;
+
+    constructor(value: string) {
+        this.value = value;
+    }
+
+    public getValue(): string {
+        return this.value;
+    }
+}
