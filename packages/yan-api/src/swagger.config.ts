8a35e7a9b01a4950f8de4abdf5188dc7865c991f
Binh Nguyen Van <nvbinhsoft@gmail.com>
2024-08-24 23:03:58 +0700

    chore: update swagger config

--- packages/yan-api/src/swagger.config.ts
+++ packages/yan-api/src/swagger.config.ts
@@ -7,6 +7,15 @@ export async function swaggerConfig(app: INestApplication) {
     .setDescription('Yan Flashcards API Documentation 📝📝')
     .addBearerAuth()
     .setVersion('1.0')
+    .addGlobalParameters({
+      in: 'header',
+      required: true,
+      name: 'Authorization',
+      schema: {
+        example:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+      },
+    })
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
