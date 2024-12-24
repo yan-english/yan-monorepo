d4f5610aa7600b7510b7c87815de3c13dd931e06
Binh Nguyen Van <nvbinhsoft@gmail.com>
2024-12-25 00:41:27 +0700

    refactor: restructure yan web

--- packages/yan-web/src/components/pages/HomeTemplate.tsx
+++
@@ -1,9 +0,0 @@
-import {Outlet} from "react-router-dom";
-
-function HomeTemplate () {
-    return (
-        <><Outlet /></>
-    );
-}
-
-export default HomeTemplate;
--- packages/yan-web/src/components/pages/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function NotFound() {
-    return (
-        <div>
-            <h1>404</h1>
-            <h2>Page not found</h2>
-        </div>
-    )
-}
