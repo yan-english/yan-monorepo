ea319696605a265ceb0d43baecd0107512419e16
Binh Nguyen Van <nvbinhsoft@gmail.com>
2024-12-25 00:42:57 +0700

    feat: create sign up page style

---
+++ packages/yan-web/src/pages/auth/signin/style.ts
@@ -0,0 +1,25 @@
+export const signInStyles = {
+    paper: {
+        p: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        transition: 'transform 0.2s',
+        '&:hover': {
+            transform: 'scale(1.01)',
+        },
+    },
+    form: {
+        width: '100%',
+        mt: 3,
+    },
+    submit: {
+        mt: 3,
+        mb: 2,
+        height: 48,
+    },
+    signUpLink: {
+        mt: 2,
+        textAlign: 'center',
+    },
+};
